387279aa3427e1b1c7964c5a09a08f29f642e5c4
Anton Pokhilenko <anton.pokhilenko@gconnect.ch>
2017-09-27 15:02:20 +0300

    new svg generator

--- test.js
+++ test.js
@@ -1,17 +1,85 @@
 const obj = require('./json.json');
+const fs = require('fs');
 
-const processObjects = objects => {
-  for (let i = 0; i < objects.length; i++) {
-    const obj = objects[i];
-    if(obj.type === 'group' && obj.row && obj.seat){
-      let svgChunk;
-      const circle = obj.objects.filter(obj => obj.type === 'circle')[0];
-      svgChunk = `<circle cx="${obj.left}" cy="${obj.top}" data-row="${obj.row}" data-seat="${obj.seat}" data-id="${obj.ticketId}" r="${circle.radius}"></circle>`;
-      console.log(svgChunk)
-    } else  if(obj.objects){
-      processObjects(obj.objects);
+normalizeGroups = (groups) => {
+  const offsets = groups.reduce((acc, group) => {
+    const { left, top } = group;
+    if (left < acc[0]) {
+      acc[0] = left;
     }
-  }
+    if (top < acc[1]) {
+      acc[1] = top;
+    }
+
+    return acc;
+  }, [0, 0]);
+
+  const [offsetX, offsetY] = offsets;
+
+  return groups.map((group) => Object.assign({}, group, { left: group.left - offsetX }, { top: group.top - offsetY }))
 }
 
-processObjects(obj.objects);
+calculateSizes = (groups) =>
+  groups.reduce((acc, group) => {
+    const { left, top, width, height } = group;
+    if (left + width > acc[0]) {
+      acc[0] = left + width;
+    }
+    if (top + height > acc[1]) {
+      acc[1] = top + height;
+    }
+
+    return acc;
+  }, [0, 0]);
+
+
+processTopLevel = ([stage, ...groups]) => {
+  const normalizedGroups = normalizeGroups(groups);
+  const [width, height] = calculateSizes(normalizedGroups);
+  const result = normalizedGroups.reduce((acc, group) => {
+    acc += processTopLevelGroup(group);
+
+    return acc;
+  }, '');
+
+  fs.writeFile('result.svg', `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">${result}</svg>`, 'utf8');
+}
+
+processTopLevelGroup = (group) => {
+  const { left, top, width, height, objects } = group;
+  const normalizedGroups = normalizeGroups(objects);
+
+  const result = normalizedGroups.reduce((acc, el) => {
+    if (el.objects.length > 1) {
+      acc += processCircleGroup(el, left, top);
+    } else if (el.objects.length === 1) {
+      acc += processRowGroup(el, left, top);
+    }
+
+    return acc;
+  }, '');
+  const rect = `<rect x="${left}" y="${top}" width="${width}" height="${height}" stroke="black" fill="transparent" stroke-width="3"/>`;
+
+  return result + rect;
+}
+
+processCircleGroup = (group, groupX, groupY) => {
+  const { left, top, row, seat, ticketId } = group;
+  const [circle] = group.objects;
+  const { radius } = circle;
+  const svgChunk = `<circle cx="${left + groupX + radius}" cy="${top + groupY + radius}" data-row="${row}" data-seat="${seat}" data-id="${ticketId}" r="${radius}"></circle>`;
+
+  return svgChunk;
+}
+
+processRowGroup = (group, groupX, groupY) => {
+  const { left, top } = group;
+  const [textNode] = group.objects;
+  const { text, height } = textNode;
+  const svgChunk = `<text x="${left + groupX + height / 2}" y="${top + groupY + height / 2}">${text}</text>`;
+
+  return svgChunk;
+}
+
+processTopLevel(obj.objects)
+
